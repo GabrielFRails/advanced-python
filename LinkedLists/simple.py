d7a1f1375de61401a85be03dc330990e9706a0d8
GabrielFRails <gabriel.estudy.reis@gmail.com>
2025-01-17 12:12:20 -0300

    Add append_index method

--- LinkedLists/simple.py
+++ LinkedLists/simple.py
@@ -7,17 +7,39 @@ class SimpleLinkedList:
 	def __init__(self):
 		self.head = None
 		self.tail = None
+		self.size = None
 	
 	def append_tail(self, data):
 		new_node = Node(data)
 		if not self.head:
 			self.head = new_node
 			self.tail = new_node
+			self.size = 1
 			return
 		
 		current = self.tail
 		current.next = new_node
 		self.tail = new_node
+		self.size = self.size + 1
+	
+	# O(n) for now
+	def append_index(self, data, index):
+		new_node = Node(data)
+		if index == 0:
+			new_node.next = self.head
+			self.head = new_node
+			return
+		
+		if index > (self.size - 1):
+			self.append_tail(data)
+			return
+
+		current = self.head
+		for i in range(index - 1):
+			current = current.next
+
+		new_node.next = current.next
+		current.next = new_node
 	
 	def display(self):
 		elements = []
@@ -33,6 +55,7 @@ def main():
 	for i in range(0, 10):
 		myll.append_tail(i)
 	
+	myll.append_index(99, 1)
 	myll.display()
 
 if __name__ == "__main__":
