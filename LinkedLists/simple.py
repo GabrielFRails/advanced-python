3b18ce8ee895abd3b38c8b4dccdb21459a35e8d1
GabrielFRails <gabriel.estudy.reis@gmail.com>
2025-01-17 10:32:03 -0300

    Initial work

---
+++ LinkedLists/simple.py
@@ -0,0 +1,14 @@
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class SimpleLinkedList:
+	def __init__(self):
+		self.head = None
+	
+	def append(self, data):
+		new_node = Node(data)
+		if not self.head:
+			self.head = new_node
+			
