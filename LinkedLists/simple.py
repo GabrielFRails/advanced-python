e3924af32c96aed2ca04c26c0a0aaeeea0585857
GabrielFRails <gabriel.estudy.reis@gmail.com>
2025-01-17 11:02:53 -0300

    Complete simple example

--- LinkedLists/simple.py
+++ LinkedLists/simple.py
@@ -11,4 +11,29 @@ class SimpleLinkedList:
 		new_node = Node(data)
 		if not self.head:
 			self.head = new_node
+			return
+		
+		current = self.head
+		while current.next:
+			current = current.next
+		current.next = new_node
+	
+	def display(self):
+		elements = []
+		current = self.head
+		while current:
+			elements.append(current.data)
+			current = current.next
+		
+		print(' -> '.join(map(str, elements)))
+
+def main():
+	myll = SimpleLinkedList()
+	for i in range(0, 10):
+		myll.append(i)
+	
+	myll.display()
+
+if __name__ == "__main__":
+	main()
 			
